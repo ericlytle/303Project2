ff3fd60219be0e67513d8544fa9062ef1cbef684
ericlytle <elmp7@mail.umkc.edu>
2015-03-25 14:21:34 -0500

    Book Class

--- Project2B-1/Book.h
+++ Project2B-1/Book.h
@@ -2,6 +2,7 @@
 
 #include "Date.h"
 #include "EmployeeQueue.h"
+#include "Employee.h"
 
 using namespace std;
 
@@ -13,16 +14,53 @@ public:
 	bool IsArchived();
 	Date CirculationStart();
 	Date CirculationStop();
-	string Name();
+	string GetName();
 	void CirculationStart(Date date);
 	void CirculationStop(Date date);
 	void Archive();
-	void Name(string name);
 	void PassOn();
+	void AddtoQueue(Employee* employee); //called by addEmployee in library
+	void PopulateQueue(list<Employee> employeeList); //only to be called by addBook in library
 private:
 	bool _isArchived;
 	Date _circulationStart;
 	Date _circulationStop;
-	EmployeeQueue _waitingForThisBook;
+	EmployeeQueue _waitingForThisBook; //pointers
 	string _name;
 };
+
+Book::Book()
+{
+	_isArchived = false;
+	_circulationStart = Date(2100, 1, 1); 
+	_circulationStop = Date(2100, 1, 1);
+	_name = "";
+}
+
+Book::Book(string name)
+{
+	_isArchived = false;
+	_circulationStart = Date(2100, 1, 1);
+	_circulationStop = Date(2100, 1, 1);
+	_name = name;
+}
+
+
+void Book::AddtoQueue(Employee* employee)
+{
+	_waitingForThisBook.Push(employee);
+}
+
+void Book::PopulateQueue(list<Employee> employeeList)
+{
+	list<Employee>::iterator employeeIterator = employeeList.begin();
+	while (employeeIterator != employeeList.end())
+	{
+		_waitingForThisBook.Push(&(*employeeIterator));
+	}
+}
+
+void Book::Archive()
+{
+	_isArchived = true;
+}

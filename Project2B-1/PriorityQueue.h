ada2275e2546baf2172e1a3208475532ad8f14a8
bradvanwick <cbvzq3@mail.umkc.edu>
2015-04-01 18:36:24 -0500

    removed empty check for PriorityQueue.Pop()

--- Project2B-1/PriorityQueue.h
+++ Project2B-1/PriorityQueue.h
@@ -4,7 +4,6 @@
 
 // PriorityQueue class
 // Uses a list to implement a priority queue.
-// Always returns largest member.
 
 template <typename T>
 class PriorityQueue
@@ -22,21 +21,17 @@ private:
 template <typename T>
 T PriorityQueue<T>::Pop()
 {
-	if (!_list.empty())
+	T top = _list.front();
+	list<T>::iterator iter = _list.begin();
+	list<T>::iterator index = _list.begin();
+	for (iter; iter != _list.end(); ++iter)
 	{
-		T top = _list.front();
-		list<T>::iterator iter = _list.begin();
-		list<T>::iterator index = _list.begin();
-		for (iter; iter != _list.end(); ++iter)
+		if (top < *iter)
 		{
-			if (top < *iter)
-			{
-				top = *iter;
-				index = iter;
-			}
+			top = *iter;
+			index = iter;
 		}
-		_list.erase(index);
-		return top;
 	}
-	throw exception("EMPTY PRIORITY_QUEUE");
+	_list.erase(index);
+	return top;
 }
